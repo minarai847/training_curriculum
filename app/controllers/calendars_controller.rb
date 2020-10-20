da1135f3b1c732dd0c4bc84a7258d701a5bb6c76
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-20 17:08:52 +0900

    issue4~6
    This reverts commit 07019bc08b20a89bb0d8471366cb3d699e557127.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -34,9 +34,23 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      wday_num = Date.today.wday + x
+      if wday_num >= 7
+        wday_num = wday_num - 7
+      end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wdays[(@todays_date+x).wday] }
+      # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wdays[wday_num] }
+
       @week_days.push(days)
     end
-
   end
 end
+
+# 38行目に7以上の場合という条件式を記述
+# ハッシュロケットかシンボルかを統一させる
+# 配列に添字として当てる記述の修正
+# →全ての日付に（火）が表示できる
+
+
+
